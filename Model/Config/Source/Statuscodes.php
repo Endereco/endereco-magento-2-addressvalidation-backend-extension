619b186c531150fc306bf448b82f45f1a1064924
Luisa123 <loechsner@parc-network.com>
2026-05-29 08:55:31 +0200

    chore: switch option sources to OptionSourceInterface
    Magento\Framework\Option\ArrayInterface has been deprecated for
    years in favour of Magento\Framework\Data\OptionSourceInterface.
    Switch both config sources over, add return-type hints on
    toOptionArray(), and wrap labels in __() for translation.
    
    Also tighten the Statuscodes source: the status-code list lived as
    35 hard-coded array literals; collapse it into a single CODES
    constant that the option array is built from.
    
    Refs #18
    Refs #7

--- Model/Config/Source/Statuscodes.php
+++ Model/Config/Source/Statuscodes.php
@@ -3,161 +3,65 @@ declare(strict_types=1);
 
 namespace Parc\AddressValidation\Model\Config\Source;
 
-use Magento\Framework\Option\ArrayInterface;
+use Magento\Framework\Data\OptionSourceInterface;
 
-class Statuscodes implements ArrayInterface
+class Statuscodes implements OptionSourceInterface
 {
-    public function toOptionArray()
+    /**
+     * Endereco API status codes available for the "manual review" trigger.
+     * Labels are wrapped in __() so installations can localise the wording,
+     * but typically these stay as identifiers in the admin UI.
+     */
+    private const CODES = [
+        'address_correct',
+        'address_multiple_variants',
+        'address_not_found',
+        'country_code_needs_correction',
+        'country_code_correct',
+        'subdivision_code_needs_correction',
+        'subdivision_code_correct',
+        'postal_code_needs_correction',
+        'postal_code_correct',
+        'locality_needs_correction',
+        'locality_correct',
+        'street_name_needs_correction',
+        'street_name_correct',
+        'street_full_needs_correction',
+        'street_full_correct',
+        'building_number_needs_correction',
+        'building_number_correct',
+        'building_number_is_missing',
+        'building_number_not_found',
+        'additional_info_needs_correction',
+        'additional_info_correct',
+        'address_is_packstation',
+        'address_is_postoffice',
+        'address_minor_correction',
+        'address_major_correction',
+        'country_code_minor_correction',
+        'country_code_major_correction',
+        'subdivision_code_minor_correction',
+        'subdivision_code_major_correction',
+        'postal_code_minor_correction',
+        'postal_code_major_correction',
+        'locality_minor_correction',
+        'locality_major_correction',
+        'street_name_minor_correction',
+        'street_name_major_correction',
+        'building_number_minor_correction',
+        'building_number_major_correction',
+    ];
+
+    public function toOptionArray(): array
     {
-        return [
-            [
-                'label' => 'address_correct',
-                'value' => 'address_correct'
-            ],
-            [
-                'label' => 'address_multiple_variants',
-                'value' => 'address_multiple_variants'
-            ],
-            [
-                'label' => 'address_not_found',
-                'value' => 'address_not_found'
-            ],
-            [
-                'label' => 'country_code_needs_correction',
-                'value' => 'country_code_needs_correction'
-            ],
-            [
-                'label' => 'country_code_correct',
-                'value' => 'country_code_correct'
-            ],
-            [
-                'label' => 'subdivision_code_needs_correction',
-                'value' => 'subdivision_code_needs_correction'
-            ],
-            [
-                'label' => 'subdivision_code_correct',
-                'value' => 'subdivision_code_correct'
-            ],
-            [
-                'label' => 'postal_code_needs_correction',
-                'value' => 'postal_code_needs_correction'
-            ],
-            [
-                'label' => 'postal_code_correct',
-                'value' => 'postal_code_correct'
-            ],
-            [
-                'label' => 'locality_needs_correction',
-                'value' => 'locality_needs_correction'
-            ],
-            [
-                'label' => 'locality_correct',
-                'value' => 'locality_correct'
-            ],
-            [
-                'label' => 'street_name_needs_correction',
-                'value' => 'street_name_needs_correction'
-            ],
-            [
-                'label' => 'street_name_correct',
-                'value' => 'street_name_correct'
-            ],
-            [
-                'label' => 'street_full_needs_correction',
-                'value' => 'street_full_needs_correction'
-            ],
-            [
-                'label' => 'street_full_correct',
-                'value' => 'street_full_correct'
-            ],
-            [
-                'label' => 'building_number_needs_correction',
-                'value' => 'building_number_needs_correction'
-            ],
-            [
-                'label' => 'building_number_correct',
-                'value' => 'building_number_correct'
-            ],
-            [
-                'label' => 'building_number_is_missing',
-                'value' => 'building_number_is_missing'
-            ],
-            [
-                'label' => 'building_number_not_found',
-                'value' => 'building_number_not_found'
-            ],
-            [
-                'label' => 'additional_info_needs_correction',
-                'value' => 'additional_info_needs_correction'
-            ],
-            [
-                'label' => 'additional_info_correct',
-                'value' => 'additional_info_correct'
-            ],
-            [
-                'label' => 'address_is_packstation',
-                'value' => 'address_is_packstation'
-            ],
-            [
-                'label' => 'address_is_postoffice',
-                'value' => 'address_is_postoffice'
-            ],
-            [
-                'label' => 'address_minor_correction',
-                'value' => 'address_minor_correction'
-            ],
-            [
-                'label' => 'address_major_correction',
-                'value' => 'address_major_correction'
-            ],
-            [
-                'label' => 'country_code_minor_correction',
-                'value' => 'country_code_minor_correction'
-            ],
-            [
-                'label' => 'country_code_major_correction',
-                'value' => 'country_code_major_correction'
-            ],
-            [
-                'label' => 'subdivision_code_minor_correction',
-                'value' => 'subdivision_code_minor_correction'
-            ],
-            [
-                'label' => 'subdivision_code_major_correction',
-                'value' => 'subdivision_code_major_correction'
-            ],
-            [
-                'label' => 'postal_code_minor_correction',
-                'value' => 'postal_code_minor_correction'
-            ],
-            [
-                'label' => 'postal_code_major_correction',
-                'value' => 'postal_code_major_correction'
-            ],
-            [
-                'label' => 'locality_minor_correction',
-                'value' => 'locality_minor_correction'
-            ],
-            [
-                'label' => 'locality_major_correction',
-                'value' => 'locality_major_correction'
-            ],
-            [
-                'label' => 'street_name_minor_correction',
-                'value' => 'street_name_minor_correction'
-            ],
-            [
-                'label' => 'street_name_major_correction',
-                'value' => 'street_name_major_correction'
-            ],
-            [
-                'label' => 'building_number_minor_correction',
-                'value' => 'building_number_minor_correction'
-            ],
-            [
-                'label' => 'building_number_major_correction',
-                'value' => 'building_number_major_correction'
-            ]
-        ];
+        $options = [];
+        foreach (self::CODES as $code) {
+            $options[] = [
+                'label' => __($code),
+                'value' => $code,
+            ];
+        }
+
+        return $options;
     }
 }
